c84767ec258473a3cc34790273694477e9507c29
Solariar <sol.forseti@gmail.com>
2023-09-21 19:28:47 +0300

    jotain hauskaa

--- kiva peli/src/App.jsx
+++ kiva peli/src/App.jsx
@@ -2,15 +2,47 @@ import Card from './assets/components/Card'
 import './app.css'
 const playerCard = {
     image: "http://placekitten.com/120/100",
-    stats: [{name:"kitty power", value: 10}]
+    stats: [{name:'kitty power', value: 16},
+    {name:'speed', value:56}]
+        }
+
+const opponentCard = {
+    image: "http://placekitten.com/120/100",
+    stats: [{name: 'kitty power', value: 15},
+    {name:'speed', value: 55}]
+    
 }
+           
+
 export default function App(){
+  function compareCards(){
+    const playerStats = playerCard.stats[0];
+    const opponentStats = opponentCard.stats[0];
+    let result = '';
+
+    if(playerStats.value === opponentStats.value){
+        result = "draw";
+    }
+    else if(playerStats.value > opponentStats.value){
+     result = "you win";
+    }
+    else{
+        result = "you lost"
+    }
+
+    console.log(result);
+    
+    
+    console.log("Button Destroyed!")
+  }
+
+
     return(
     <> 
         <h1>hello world!</h1>
-        <Card card={playerCard}>
-
-        </Card>
+        <Card card={playerCard}></Card>
+        <button type='button' onClick={compareCards}>Play</button>
+        <Card card={opponentCard}></Card>
      </>   
     );
   
--- kiva peli/src/assets/components/Card.jsx
+++ kiva peli/src/assets/components/Card.jsx
@@ -3,10 +3,13 @@ export default function Card({card}){
         <div className="card">
         <img src="http://placekitten.com/120/100"/>
         <ul className="stat.list">
-           <li className="stat-list-item">
-                <span>kitty power</span>
-                <span> 9</span>
-</li>
+           {card.stats.map((stat,index) =>(
+           <li className="stat-list-item" key={index}>
+                <span>{stat.name}</span>
+                <span>{stat.value}</span>   
+
+            </li>       
+           ))}
         </ul>
             
         </div>
